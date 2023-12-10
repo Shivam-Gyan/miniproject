604d4d3b243ed307fa23a7e782fc8dc5cbeeff5e
utkbella <114491739+utkbella@users.noreply.github.com>
2023-12-10 17:18:22 +0530

    Server setup for index

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
 
-const https=require("https");
-
 const   bodyParser=require("body-parser");
-const { log } = require("console");
-app.use(bodyParser.urlencoded({extended:true}));
-
 
 const app = express();
+app.set('view engine','ejs');
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+app.get("/",function(req,res){
+
+  res.render("index");
+})
+
+
+
+
+app.listen(3000, () => {
+  console.log(`Server started on port 3000`);
+}); 
